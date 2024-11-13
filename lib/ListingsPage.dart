8e5320c6482c3b93cdc5cd3b855117017c347496
HatNJacket <nickdrapak@yahoo.ca>
2024-11-13 18:23:21 -0500

    Fixed grammar from last commit

--- lib/ListingsPage.dart
+++ lib/ListingsPage.dart
@@ -19,13 +19,13 @@ class ListingsPageState extends State<ListingsPage> {
   //TODO: Replace temporary listings list with SQL or web database
   final List<Listing> listings = [
     Listing(title: 'House 1',
-    description: 'It an house',
+    description: "It's a house",
     imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwNUhxbHpwCgZLNYYRF4JMfbhKQ-VQVMQRUA&s',
     price: 100000.01,
     moreInfo: 'Small house',
     ),
     Listing(title: 'House 2',
-    description: 'It an different house',
+    description: "It's a different house",
     imageURL: 'https://i.ytimg.com/vi/_L6jEtMK8No/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD3Jf8E6GHx6CjfSmFk80hileTi_A',
     price: 199999.99,
     moreInfo: 'Bigger house',
